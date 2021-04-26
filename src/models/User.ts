2d04a78fedf2b8a82756e1f7d27e77da06f55138
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2021-04-26 02:59:42 -0300

    Added the specific User interface, with the attributes that compound this type.

---
+++ src/models/User.ts
@@ -0,0 +1,12 @@
+// Specifies which attributes an User must have.
+
+interface User {
+	id: string;
+	name: string;
+	phone: string;
+	email: string;
+	age: number;
+	weight: number;
+}
+
+export default User;
